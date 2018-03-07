817a7f3f1e9415378795dbe3e4386f441a2ebef2
realityhas <rubenfolini@gmail.com>
2018-03-07 15:19:46 +0100

    I'm able to add condition expressions to outgoing flows

--- transformation2/src/main/java/bpmn/transformation2/Rule3.java
+++ transformation2/src/main/java/bpmn/transformation2/Rule3.java
@@ -10,6 +10,7 @@ import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 import org.camunda.bpm.model.bpmn.instance.Activity;
 import org.camunda.bpm.model.bpmn.instance.BpmnModelElementInstance;
+import org.camunda.bpm.model.bpmn.instance.ConditionExpression;
 import org.camunda.bpm.model.bpmn.instance.ExclusiveGateway;
 import org.camunda.bpm.model.bpmn.instance.ParallelGateway;
 import org.camunda.bpm.model.bpmn.instance.ScriptTask;
@@ -22,69 +23,78 @@ public class Rule3 {
 
 	public static void main(String[] args) {
 
+		//String path = askForPath();
+		
 		String path = "/Users/rubenfolini/Desktop/Archive/Parallel/1.1.Parallel_Multiple.bpmn.xml";
 		//String path = "/Users/rubenfolini/Desktop/Archive/Parallel/1.2.2 Parallel_R1R3.bpmn.xml"
-
+		//TODO having a way to use all of the bpmn files that are found inside a single folder.
 		File bpmnFile = new File(path);
 
-		String filename = bpmnFile.getName().replace(".bpmn.xml", ""); //this will be used later to replace it with the new one more easily. 
+		String filename = bpmnFile.getName().replace(".bpmn.xml", ""); //this variable is used later to replace it with the new one more easily. 
 
 		//Creating input and output models
 		BpmnModelInstance inputModelInstance = Bpmn.readModelFromFile(bpmnFile);
-		BpmnModelInstance outputModelInstance = Bpmn.readModelFromFile(bpmnFile); // here I can't just say output = input because those are object and it would just copy the address right?
 
 		//Applying rule 3 to outputModelInstance:
-		rule3(inputModelInstance,outputModelInstance);
+		rule3(inputModelInstance);
 		//TODO I don't need outputModelInstance, i can just save the old instance as a different file.
 
 
 		//Writing the output model to file
-		writeModeltoFile(outputModelInstance, filename);
+		writeModeltoFile(inputModelInstance, filename);
 	}
-	public void askForPath() {
-		
+
+	//This lets the user decide the path of the file
+	//TODO provide exceptions? What happens if it's not the correct path?
+	public static String askForPath() {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Please enter the location of the bpmn file you wan to transform:");
+		String filePath = reader.next();
+		reader.close();
+		return filePath;
 	}
-	public static void rule3 (BpmnModelInstance inputModel, BpmnModelInstance outputModel) {
 
-		/*Why do I have to extract the ParallelGatway type from an instance of a model? 
-		Don't I have an abstract "idea" of what a ParallelGateway is?*/ 
-		/*And why does it works even if I'm getting a type from a model that has no gateway of the requested type?*/
-
-		//Extracting the types of elements that i need
-
-		//TODO when I'm done, remove the ones that I didn't use
+	public static void rule3 (BpmnModelInstance inputModel) {
 
 		//Here I'm creating a collection of all the parallelGateways in the outputModelInstance (which for now its identical to the starting model)
-		Collection<ParallelGateway> parallelGatewayInstances = outputModel.getModelElementsByType(ParallelGateway.class);
-		
+		Collection<ParallelGateway> parallelGatewayInstances = inputModel.getModelElementsByType(ParallelGateway.class);
+
+		//The following counters serves us to understand if I'm reading the inputModel correctly.
 		int parallelGatewayCounter = 0; 
 		int outflowsFromParaGateCounter = 0;
-		
+
 		//going through all of the parallelGateways in the model:
-		for(ModelElementInstance oldParallelGateway : parallelGatewayInstances){
-			
+		for(ParallelGateway oldParallelGateway : parallelGatewayInstances){
+
 			//I use this just for counting. E.G. I know that in this model there are 6 ParaGateways so I expect C to go to 6
 			parallelGatewayCounter++;
-			//System.out.println("Parallel Gateway Number " + parallelGatewayCounter);
+			System.out.println("Parallel Gateway Number " + parallelGatewayCounter);
 
 			//creating the substitute element
-			InclusiveGateway newInclusiveGateway = outputModel.newInstance(InclusiveGateway.class);
-			//BpmnModelElementInstance newInclusiveGateway =  outputModel.newInstance(inclusiveGatewayType);
+			InclusiveGateway newInclusiveGateway = inputModel.newInstance(InclusiveGateway.class);
 			//giving it the "id" of the old gateway
 			newInclusiveGateway.setAttributeValue("id", oldParallelGateway.getAttributeValue("id"), true);
+
 			//finally replacing the element with its substitute.
 			oldParallelGateway.replaceWithElement(newInclusiveGateway);
-			//After this point, what should I use to look for the outgoing flows? The Old or the New?
-			
 			newInclusiveGateway.updateAfterReplacement();			
-			//Finding All Outgoing Flows ***THIS DOES NOT WORK:*** :
 			
-			//Collection<ModelElementInstance> outgoingFlowsInstances = oldParallelGateway.getChildElementsByType(sequenceFlowType);
+			//Now for each newly created InclusiveGateway I'm going to add conditional expressions that always evaluate to TRUE
+			//first I'm adding all of them to a collection:
 			Collection<SequenceFlow> outgoingFlowsInstances = newInclusiveGateway.getOutgoing();
-			//Here I'm trying to understand what getChildElement does. Does it returns the flows or the elements that follow? It doesnt seem to do either.
-			for(ModelElementInstance outgoingFlows : outgoingFlowsInstances) {
+			for(SequenceFlow outgoingFlow : outgoingFlowsInstances) {
+				
+				//I use this just for counting. E.G. I know that the first ParallelGateway has two outgoing flows, so i expect this number to go to 2.
 				outflowsFromParaGateCounter++;
-				System.out.println(" Outflows From Gateway number " + parallelGatewayCounter+ ": " + outflowsFromParaGateCounter);
+				System.out.println("    Outflows: " + outflowsFromParaGateCounter);
+				
+				//I'm creating a new conditionExpression:
+				ConditionExpression trueConditionExpression = inputModel.newInstance(ConditionExpression.class);
+				trueConditionExpression.setType("tFormalExpression");
+				//As the name suggest, the condition must always be true:
+				trueConditionExpression.setTextContent("${1 == 1}");
+				//I'm adding the condition to my outgoingFlow
+				outgoingFlow.setConditionExpression(trueConditionExpression);
 			}
 		}
 	}
