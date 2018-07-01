e4140b4e88aef594eb77b53b5b2e794e2ac2dd3a
realityhas <rubenfolini@gmail.com>
2018-07-01 19:59:13 +0200

    Updates on the Travel class

--- transformation2/src/main/java/bpmn/transformation2/Travel.java
+++ transformation2/src/main/java/bpmn/transformation2/Travel.java
@@ -1,14 +1,12 @@
 package bpmn.transformation2;
 
-import java.util.ArrayList;
+import java.util.ArrayList;import javax.swing.text.AbstractDocument.LeafElement;
 import javax.ws.rs.GET;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.apache.ibatis.annotations.ConstructorArgs;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-
-
 
 
 public class Travel {
@@ -22,7 +20,7 @@ public class Travel {
      * @param model
      */
     public Travel (Model model) {
-	
+
 	this.model = model;
 	paths = new ArrayList<ArrayList<Element>>();
 	visited = new ArrayList<String>();
@@ -32,40 +30,41 @@ public class Travel {
      * From a starting point element, get all the possible paths
      * @throws XPathExpressionException 
      */
-    public ArrayList <Element> getAPath (Element startingPoint) throws XPathExpressionException{
+    public ArrayList <Element> getAPath (ArrayList<Element> past) throws XPathExpressionException{
 	ArrayList<String> visited = new ArrayList<String>(); //contains the ID's of the visited elements
-	ArrayList<Element> deepSuccessors = new ArrayList<Element>();
-
+	ArrayList<Element> path = new ArrayList<Element>();
+	
+	
+	Element startingPoint = past.get(past.size()-1); //we start from the last element of this path
+	
 	System.out.println("Visiting element " + startingPoint.getAttribute("id"));
 	//adding the startingPoint to the list of visited elements
-	visited.add(startingPoint.getAttribute("id")); 
-
+	visited.add(startingPoint.getAttribute("id"));
+	
 	// getting the successors of our starting point
 	ArrayList<Element> immediateSuccessors = model.getSuccessors(startingPoint);
-
-	//Adding the successors to the list of deepSuccessors
-	for (Element successor : immediateSuccessors) {
-	    deepSuccessors.add(successor);
-	}
-
+	
+	
 	//going through all the immediate successors
 	for (Element successor : immediateSuccessors)
 	    //checking if the immediateSuccessors has been visited
 	    if (!visited.contains(successor.getAttribute("id"))) {
-		paths.add(getAPath(successor));
-
+		past.add(successor);
+		getAPath(past);
 	    }
-
-
-
-	//TODO:
-	// Aggiungi startingPoint alla lista dei visited,
-	//poi fai getSuccessors, poi per ogni successor lo aggiungi alla lista dei deepFollowers,
-	// poi usi il metodo getdeepsuccessors (questo) e appendi il risultato alla lista dei deepFollowers.
-	//  SOLO se non è stato visitato, lo aggiungi alla lista dei visited.
+	
+	
+	if (immediateSuccessors.size() == 0) {
+	    System.out.println("I've reached the end of the path");
+	    paths.add(past);
+	}
+	
+	
+	//poi usi il metodo getdeepsuccessors (questo) e appendi il risultato alla lista dei deepFollowers.
+	//SOLO se non è stato visitato, lo aggiungi alla lista dei visited.
 	//Quando c'è un IF crei un nuovo array (che corrisponde a un nuovo path), però prima copi il contenuto 
 	//del (singolo) path che c'era fino a quel momento. Altrimenti se non lo fai partiresti da troppo dopo
-	return deepSuccessors;
+	return path;
 
     }
 
@@ -74,30 +73,36 @@ public class Travel {
      */
     public ArrayList <Element> mandatoryDeepSuccessors (ArrayList paths){
 	ArrayList <Element> mandatoryDeepSuccessors = new ArrayList<Element>();
-
-
+	//TODO
 	return mandatoryDeepSuccessors;
 
     }
-    
-    
+
+
+    /**
+     * Navigate a model from its starting point
+     * @throws XPathExpressionException
+     */
     public void travelModel() throws XPathExpressionException {
 	//Finding the start element
 	Element start = (Element) model.doc.getElementsByTagName("bpmn:startEvent").item(0);
-	
 	//Getting paths from the start element:
-	getAPath(start);
 	
+	ArrayList<Element> startingList = new ArrayList<Element>();
+	
+	startingList.add(start);
+	getAPath(startingList);
+
     }
-    
+
     /**
      * Used for testing
      * That's why for now I'm using names instead of id, but that
      * shall be changed (TODO)
      */
     public void printPaths() {
-	System.out.println("=====PRINTING PATHS=====");
-	
+	System.out.println("======= PRINTING PATHS =======");
+
 	for (ArrayList<Element> path : paths) {
 	    for (Element element : path) {
 		System.out.println(element.getAttribute("name"));
