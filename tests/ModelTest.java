c222e659f63652c13dd615e0637015d0f691b054
rubenfolini <rubenfolini@gmail.com>
2018-11-28 18:03:52 +0100

    Added basic test models for the rules.

---
+++ tests/ModelTest.java
@@ -0,0 +1,386 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelTest {
+
+
+    @Test
+    void getDoc () {
+
+    }
+
+
+    @Test
+    void setDoc () {
+
+    }
+
+
+    @Test
+    void getProcess () {
+
+    }
+
+
+    @Test
+    void setProcess () {
+
+    }
+
+
+    @Test
+    void getBpmndiDiagram () {
+
+    }
+
+
+    @Test
+    void setBpmndiDiagram () {
+
+    }
+
+
+    @Test
+    void getBpmndiPlane () {
+
+    }
+
+
+    @Test
+    void setBpmndiPlane () {
+
+    }
+
+
+    @Test
+    void getTagStyle () {
+
+    }
+
+
+    @Test
+    void setTagStyle () {
+
+    }
+
+
+    @Test
+    void getDoc1 () {
+
+    }
+
+
+    @Test
+    void setDoc1 () {
+
+    }
+
+
+    @Test
+    void getProcess1 () {
+
+    }
+
+
+    @Test
+    void setProcess1 () {
+
+    }
+
+
+    @Test
+    void getBpmndiDiagram1 () {
+
+    }
+
+
+    @Test
+    void setBpmndiDiagram1 () {
+
+    }
+
+
+    @Test
+    void getBpmndiPlane1 () {
+
+    }
+
+
+    @Test
+    void setBpmndiPlane1 () {
+
+    }
+
+
+    @Test
+    void getTagStyle1 () {
+
+    }
+
+
+    @Test
+    void setTagStyle1 () {
+
+    }
+
+
+    @Test
+    void getPath () {
+
+    }
+
+
+    @Test
+    void setPath () {
+
+    }
+
+
+    @Test
+    void getXpath () {
+
+    }
+
+
+    @Test
+    void setXpath () {
+
+    }
+
+
+    @Test
+    void getDocBuilder () {
+
+    }
+
+
+    @Test
+    void setDocBuilder () {
+
+    }
+
+
+    @Test
+    void getRulesApplied () {
+
+    }
+
+
+    @Test
+    void setRulesApplied () {
+
+    }
+
+
+    @Test
+    void cloneModel () {
+
+    }
+
+
+    @Test
+    void addRule () {
+
+    }
+
+
+    @Test
+    void addOutputInPath () {
+
+    }
+
+
+    @Test
+    void newTask () {
+
+    }
+
+
+    @Test
+    void newInclusiveGateway () {
+
+    }
+
+
+    @Test
+    void newParallelGateway () {
+
+    }
+
+
+    @Test
+    void newExclusiveGateway () {
+
+    }
+
+
+    @Test
+    void newSequenceFlow () {
+
+    }
+
+
+    @Test
+    void setSource () {
+
+    }
+
+
+    @Test
+    void getPosition () {
+
+    }
+
+
+    @Test
+    void calculatePositionOfNewNode () {
+
+    }
+
+
+    @Test
+    void calculatePositionOfNewNode1 () {
+
+    }
+
+
+    @Test
+    void setTarget () {
+
+    }
+
+
+    @Test
+    void getSuccessors () {
+
+    }
+
+
+    @Test
+    void getPredecessors () {
+
+    }
+
+
+    @Test
+    void getOutgoingFlows () {
+
+    }
+
+
+    @Test
+    void getIncomingFlows () {
+
+    }
+
+
+    @Test
+    void hasCondition () {
+
+    }
+
+
+    @Test
+    void returnConditionString () {
+
+    }
+
+
+    @Test
+    void returnConditionElement () {
+
+    }
+
+
+    @Test
+    void applyCondition () {
+
+    }
+
+
+    @Test
+    void getType () {
+
+    }
+
+
+    @Test
+    void changeType () {
+
+    }
+
+
+    @Test
+    void getTarget () {
+
+    }
+
+
+    @Test
+    void deleteIncomingFlows () {
+
+    }
+
+
+    @Test
+    void deleteOutgoingFlows () {
+
+    }
+
+
+    @Test
+    void delete () {
+
+    }
+
+
+    @Test
+    void replaceELement () {
+
+    }
+
+
+    @Test
+    void findElemById () {
+
+    }
+
+
+    @Test
+    void isAMerge () {
+
+    }
+
+
+    @Test
+    void isASplit () {
+
+    }
+
+
+    @Test
+    void isUselessGateway () {
+
+    }
+
+
+    @Test
+    void deleteUselesGateway () {
+
+    }
+
+
+    @Test
+    void style () {
+
+    }
+
+
+    @Test
+    void saveToFile () {
+
+    }
+
+
+}
