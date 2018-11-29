2c3cc6b343ac448b1ffa68a269f9414b9d2f3f02
rubenfolini <rubenfolini@gmail.com>
2018-11-29 13:25:40 +0100

    Addedd a test for Rule1

--- tests/Rule1Test.java
+++ tests/Rule1Test.java
@@ -1,12 +1,25 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class Rule1Test {
 
 
     @Test
-    void applyRule () {
+    void applyRule () throws Exception {
+
+        Model startingModel = new Model("./tests/TestModels/Rule1" +
+        "/Rule1Classic.bpmn.xml");
+
+        Model expected = new Model("./tests/TestModels/Rule1/Rule1Classic1" +
+         ".bpmn.xml");
+
+
+        Model result = startingModel.cloneModel();
+
+        Rule1.applyRule(result);
+
+        assertFalse(TravelAgency.modelsAreDifferent(result,expected));
+        assertTrue(TravelAgency.modelsAreDifferent(result, startingModel));
 
     }
 
--- tests/TravelAgencyTest.java
+++ tests/TravelAgencyTest.java
@@ -15,32 +15,22 @@ class TravelAgencyTest {
 
     }
 
-
-    @Test
-    void printMandatoryDeepSuccessors () {
-
-    }
-
-
-    @Test
-    void printNumberOfPaths () {
-
-    }
-
-
     @Test
     void modelsAreDifferent () throws ParserConfigurationException,
      SAXException, IOException, XPathExpressionException {
 
-        Model base =
-         new Model("/Users/rubenfolini/BPMNTransformations/tests/TestModels/TravelAgency/BaseModelForTA.bpmn.xml");
+        Model base = new Model("./tests/TestModels/TravelAgency" +
+        "/BaseModelForTA.bpmn.xml");
         //the model against which all others will be tested.
 
-        Model missingTask = new Model("/Users/rubenfolini/BPMNTransformations/tests/TestModels/TravelAgency/aTaskIsMissing.bpmn.xml");
+        Model missingTask = new Model("./tests/TestModels/TravelAgency" +
+        "/aTaskIsMissing.bpmn.xml");
         assertTrue(TravelAgency.modelsAreDifferent(base, missingTask));
         //This model is different from the base model: a task has been deleted.
 
-        Model expressionAdded = new Model("/Users/rubenfolini/BPMNTransformations/tests/TestModels/TravelAgency/onlyOneExpressionIsAdded.bpmn.xml");
+        Model expressionAdded =
+         new Model("./tests/TestModels/TravelAgency" +
+         "/onlyOneExpressionIsAdded.bpmn.xml");
 
         assertFalse(TravelAgency.modelsAreDifferent(base, expressionAdded));
         //This model is equal from the base model: a couple of sequenceFlows
@@ -48,14 +38,15 @@ class TravelAgencyTest {
         //TODO this should change, but if it doesn't I can justify it by saying
         // that there are no rules that only change expressions.
 
-        Model onlyPositionsAreDifferent =
-         new Model("/Users/rubenfolini/BPMNTransformations/tests/TestModels/TravelAgency/onlyPositionsAreDifferent.bpmn.xml");
+        Model onlyPositionsAreDifferent = new Model("./tests/TestModels" +
+        "/TravelAgency/onlyPositionsAreDifferent.bpmn.xml");
         assertFalse(TravelAgency.modelsAreDifferent(base,
          onlyPositionsAreDifferent));
         //The position of the BPMN symbols should not matter. As such, it
         // should be equal to the base model.
 
-        Model onlyTypesAreDifferent = new Model("/Users/rubenfolini/BPMNTransformations/tests/TestModels/TravelAgency/onlyTypesAreDifferent.bpmn.xml");
+        Model onlyTypesAreDifferent = new Model("./tests/TestModels" +
+        "/TravelAgency/onlyTypesAreDifferent.bpmn.xml");
         assertTrue(TravelAgency.modelsAreDifferent(base,
          onlyTypesAreDifferent));
         //The type of the models matters: as such it is a different model
